06e45e1656d308a5ce646407418a4f50ca1fada5
Eric Krebs <ekrebs@outlook.com>
2023-06-06 19:23:56 -0500

    update clean

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -r dbpath1 dbpath2 dbpath3 log1 log2 log3
-rm pid1.pid pid2.pid pid3.pid
+rm -r ~/mongo/dbpath1 ~/mongo/dbpath2 ~/mongo/dbpath3 ~/mongo/log1 ~/mongo/log2 ~/mongo/log3
+rm ~/mongo/pid1.pid ~/mongo/pid2.pid ~/mongo/pid3.pid
